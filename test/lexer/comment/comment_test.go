4a28b253049d17f1c3885fde6e1cbd22501177ec
Marius Ackerman <mariusackerman0@gmail.com>
2013-10-10 09:09:51 +0200

    lexer tests added.

---
+++ test/lexer/comment/comment_test.go
@@ -0,0 +1,20 @@
+package comment
+
+import (
+	"code.google.com/p/gocc/test/lexer/comment/lexer"
+	"code.google.com/p/gocc/test/lexer/comment/token"
+	"testing"
+)
+
+func Test1(t *testing.T) {
+	lex := lexer.NewLexerString("token1 ; comment \n token2 token3")
+	i, tok := 0, lex.Scan()
+	for ; tok.Type != token.EOF; i, tok = i+1, lex.Scan() {
+		if tok.Type != token.TokMap.Type("token") {
+			t.Errorf("Expected token type %d, got %d, lit=%s", token.TokMap.Type("token"), tok.Type, tok.Lit)
+		}
+	}
+	if i != 3 {
+		t.Errorf("Expected 3 tokens, got %d", i)
+	}
+}
---
+++ test/lexer/comment2/lexer/acttab.go
@@ -0,0 +1,76 @@
+package lexer
+
+import (
+	"code.google.com/p/gocc/test/lexer/comment2/token"
+	"fmt"
+)
+
+type ActionTable [NumStates]ActionRow
+
+type ActionRow struct {
+	Accept token.Type
+	Ignore string
+}
+
+func (this ActionRow) String() string {
+	return fmt.Sprintf("Accept=%d, Ignore=%s", this.Accept, this.Ignore)
+}
+
+var ActTab = ActionTable{
+	ActionRow{ // S0
+		Accept: 0,
+		Ignore: "",
+	},
+	ActionRow{ // S1
+		Accept: -1,
+		Ignore: "!ws",
+	},
+	ActionRow{ // S2
+		Accept: 0,
+		Ignore: "",
+	},
+	ActionRow{ // S3
+		Accept: 0,
+		Ignore: "",
+	},
+	ActionRow{ // S4
+		Accept: -1,
+		Ignore: "!comment",
+	},
+	ActionRow{ // S5
+		Accept: 0,
+		Ignore: "",
+	},
+	ActionRow{ // S6
+		Accept: 0,
+		Ignore: "",
+	},
+	ActionRow{ // S7
+		Accept: 0,
+		Ignore: "",
+	},
+	ActionRow{ // S8
+		Accept: 0,
+		Ignore: "",
+	},
+	ActionRow{ // S9
+		Accept: 3,
+		Ignore: "",
+	},
+	ActionRow{ // S10
+		Accept: 3,
+		Ignore: "",
+	},
+	ActionRow{ // S11
+		Accept: 3,
+		Ignore: "",
+	},
+	ActionRow{ // S12
+		Accept: 0,
+		Ignore: "",
+	},
+	ActionRow{ // S13
+		Accept: 3,
+		Ignore: "",
+	},
+}
