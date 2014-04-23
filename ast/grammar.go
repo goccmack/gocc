101acee99d75357ecdc5acd209ee2cec8a638cc9
Anton Hendriks <anton.hendriks@gmail.com>
2014-04-23 15:13:39 +0200

    added consistency check

--- ast/grammar.go
+++ ast/grammar.go
@@ -14,6 +14,12 @@
 
 package ast
 
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
 type Grammar struct {
 	*LexPart
 	*SyntaxPart
@@ -30,5 +36,54 @@ func NewGrammar(lexPart, syntaxPart interface{}) (*Grammar, error) {
 	if syntaxPart != nil {
 		g.SyntaxPart = syntaxPart.(*SyntaxPart).augment()
 	}
-	return g, nil
+
+	return g, consistent(g)
+}
+
+var (
+	errUndefined = errors.New("undefined symbol used in production")
+)
+
+func consistent(g *Grammar) (err error) {
+	if g.SyntaxPart == nil {
+		return
+	}
+
+	defs := make(map[string]bool)
+	used := make(map[string][]string)
+
+	for _, tok := range g.LexPart.TokDefsList {
+		defs[tok.id] = true
+	}
+	for _, prod := range g.SyntaxPart.ProdList {
+		defs[prod.Id] = true
+		for _, s := range prod.Body.Symbols {
+			if s.String()[0] == '"' {
+				continue
+			}
+			used[s.String()] = append(used[s.String()], prod.Id)
+		}
+	}
+	for s := range defs {
+		if s == "S'" {
+			continue
+		}
+		if _, ok := used[s]; !ok {
+			fmt.Fprintf(os.Stderr, "warning: symbol %q defined but never used\n", s)
+		}
+	}
+	for s, in := range used {
+		if _, ok := defs[s]; !ok {
+			if s == "empty" {
+				continue
+			}
+			if s[0] >= 'A' && s[0] <= 'Z' {
+				fmt.Fprintf(os.Stderr, "error: undefined symbol %q used in productions %q\n", s, in)
+				err = errUndefined
+			} else {
+				fmt.Fprintf(os.Stderr, "warning: undefined symbol %q used in productions %q\n", s, in)
+			}
+		}
+	}
+	return
 }
