5fbf58b41a6141ab814a58a425d7de5a16cca120
Daniel Podolsky <onokonem@gmail.com>
2019-03-02 22:25:02 +0300

    development: current entity name can be provided to DST function

--- internal/frontend/token/token.go
+++ internal/frontend/token/token.go
@@ -91,33 +91,14 @@ func (T *Token) UintValue() (uint64, error) {
 	return strconv.ParseUint(string(T.Lit), 10, 64)
 }
 
+var (
+	sdtPlaceholders = regexp.MustCompile(`\$(\d+)`)
+	sdtReplacement  = []byte("X[$1]")
+)
+
 func (T *Token) SDTVal() string {
-	sdt := string(T.Lit)
-	rex, err := regexp.Compile("\\$[0-9]+")
-	if err != nil {
-		panic(err)
-	}
-	idx := rex.FindAllStringIndex(sdt, -1)
-	res := ""
-	if len(idx) <= 0 {
-		res = sdt
-	} else {
-		for i, loc := range idx {
-			if loc[0] > 0 {
-				if i > 0 {
-					res += sdt[idx[i-1][1]:loc[0]]
-				} else {
-					res += sdt[0:loc[0]]
-				}
-			}
-			res += "X["
-			res += sdt[loc[0]+1 : loc[1]]
-			res += "]"
-		}
-		if idx[len(idx)-1][1] < len(sdt) {
-			res += sdt[idx[len(idx)-1][1]:]
-		}
-	}
+	res := string(sdtPlaceholders.ReplaceAll(T.Lit, sdtReplacement))
+
 	return strings.TrimSpace(res[2 : len(res)-2])
 }
 
