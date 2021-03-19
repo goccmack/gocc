5cda7017cef154b0c878c1def7fb12ed1de094d9
Oliver 'kfsone' Smith <oliver@kfs.org>
2021-03-19 13:47:21 -0700

    Regenerated files

--- example/errorrecovery/token/token.go
+++ example/errorrecovery/token/token.go
@@ -3,6 +3,7 @@
 package token
 
 import (
+	"bytes"
 	"fmt"
 	"strconv"
 	"unicode/utf8"
@@ -56,6 +57,17 @@ func (m TokenMap) TokenString(tok *Token) string {
 
 func (m TokenMap) StringType(typ Type) string {
 	return fmt.Sprintf("%s(%d)", m.Id(typ), typ)
+}
+
+// Equal implements the Equal interface for Token, returning true if the
+// token Type and Lit are matches.
+func (t *Token) Equal(rhs interface{}) bool {
+	switch rhsT := rhs.(type) {
+	case *Token:
+		return t == rhsT || (t.Type == rhsT.Type && bytes.Equal(t.Lit, rhsT.Lit))
+	default:
+		return false
+	}
 }
 
 // CharLiteralValue returns the string value of the char literal.
