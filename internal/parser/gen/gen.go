f8cc7cd8ff8c8c8c3a7fc5e3d3d82769689fb0b4
Oliver 'kfsone' Smith <oliver@kfs.org>
2021-03-22 10:49:04 -0700

    Resolve merge conflict

--- internal/parser/gen/gen.go
+++ internal/parser/gen/gen.go
@@ -12,7 +12,6 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
-
 // This package controls the generation of all parser-related code.
 package gen
 
