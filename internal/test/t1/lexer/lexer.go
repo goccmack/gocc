33c7cde9e59d8c60b582aded3b85b06f9d9fb806
Walter Schulze <awalterschulze@gmail.com>
2023-02-28 18:45:09 +0000

    replace ioutil with os

--- internal/test/t1/lexer/lexer.go
+++ internal/test/t1/lexer/lexer.go
@@ -45,7 +45,7 @@ func (s *SourceContext) Source() string {
 }
 
 func NewLexerFile(fpath string) (*Lexer, error) {
-	src, err := ioutil.ReadFile(fpath)
+	src, err := os.ReadFile(fpath)
 	if err != nil {
 		return nil, err
 	}
